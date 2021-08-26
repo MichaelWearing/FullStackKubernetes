a4461b038d07383eeb025d06350c72efaa44de6e
Mikey <mikeywearing@gmail.com>
2021-08-26 17:41:39 +0200

    First commit

---
+++ src/main/java/com/example/dockerpractice/student/StudentConfig.java
@@ -0,0 +1,32 @@
+package com.example.dockerpractice.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student Mikey = new Student(
+                    "Mikey",
+                    "mikeywearing@gmail.com",
+                    LocalDate.of(1991, Month.DECEMBER, 27)
+            );
+
+            Student Ste = new Student(
+                    "Ste",
+                    "ste@gmail.com",
+                    LocalDate.of(1993, Month.OCTOBER, 29)
+            );
+
+            repository.saveAll(List.of(Mikey, Ste));
+        };
+    }
+}
